629b71fca6488001827644ab02b311b0e7f30c54
Nelson Romero <nromero125@gmail.com>
2026-03-20 11:56:58 -0400

    excluding crsf paddle

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,10 @@ return Application::configure(basePath: dirname(__DIR__))
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
 
+        $middleware->validateCsrfTokens(except: [
+            'paddle/webhook',
+        ]);
+
         $middleware->alias([
             'subscribed' => \App\Http\Middleware\EnsureClinicSubscribed::class,
         ]);
